6c99a1402573e3990bbf9ea337c292e150b7fb99
Swastik Bhattacharyya <swastikunity@gmail.com>
2025-07-18 10:27:08 +0530

    feat: update validated action pattern to support auth session check
    Add function validatedActionWithUser which performs schema validation
    and checks the presence of a user session cookie before performing the
    server action.

---
+++ src/lib/actions.ts
@@ -0,0 +1,8 @@
+"use server";
+
+import { headers } from "next/headers";
+import { auth } from "./auth";
+
+export async function getUser() {
+  return await auth.api.getSession({ headers: await headers() });
+}
--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,9 +1,20 @@
 import toast from "react-hot-toast";
 import z from "zod";
+import { getUser } from "./actions";
 
 export type ValidatedActionResult = {
   success: boolean;
   message: string;
+};
+
+type UserType = {
+  id: string;
+  name: string;
+  emailVerified: boolean;
+  email: string;
+  createdAt: Date;
+  updatedAt: Date;
+  image?: string | null | undefined | undefined;
 };
 
 export function validatedAction<T extends z.ZodTypeAny, S = undefined>(
@@ -25,6 +36,39 @@ export function validatedAction<T extends z.ZodTypeAny, S = undefined>(
         message: parseResult.error.errors[0].message,
       };
     return await action(data, optionalParams);
+  };
+}
+
+export function validatedActionWithUser<T extends z.ZodTypeAny, S = undefined>(
+  schema: T,
+  action: S extends undefined
+    ? (data: z.infer<T>, user: UserType) => Promise<ValidatedActionResult>
+    : (
+        data: z.infer<T>,
+        user: UserType,
+        params: S,
+      ) => Promise<ValidatedActionResult>,
+) {
+  return async (
+    ...args: S extends undefined
+      ? [data: z.infer<T>]
+      : [data: z.infer<T>, params: S]
+  ): Promise<ValidatedActionResult> => {
+    const [data, optionalParams] = args as [z.infer<T>, S];
+    const parseResult = schema.safeParse(data);
+    if (!parseResult.success)
+      return {
+        success: false,
+        message: parseResult.error.errors[0].message,
+      };
+
+    const user = await getUser();
+    if (!user)
+      return {
+        success: false,
+        message: "You must be signed in to perform this action",
+      };
+    return await action(data, user.user, optionalParams);
   };
 }
 
